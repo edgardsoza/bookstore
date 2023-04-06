e14108356976193145d2e2ed4399a2a819908c91
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-04-06 16:47:52 -0600

    fix linter

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,10 @@ import { NavLink } from 'react-router-dom';
 export default function NavBar() {
   return (
     <div className="header-container">
-        <strong>
+      <strong>
         <div className="header-title">Bookstore CMS</div>
-        </strong>
-        <ul className="navbar">
+      </strong>
+      <ul className="navbar">
         <li><NavLink style={{ textDecoration: 'none' }} to="/">Home</NavLink></li>
         <li><NavLink style={{ textDecoration: 'none' }} to="/categories">Categories</NavLink></li>
       </ul>
