408b97fa49d10a788633391f56cc6c87faa7355c
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-04-06 16:14:40 -0600

    create booklist categories and header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route, Outlet, Link } from "react-router-dom";
+import BookList from './components/BookList'
+import Categories from './components/Categories'
+import NavBar from './components/Header'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <NavBar/>
+    <Routes>
+    <Route path='/' element={<BookList/>}/>
+    <Route path='/categories' element={<Categories/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Categories.js
@@ -0,0 +1,7 @@
+export default function Categories() {
+    return (
+      <div className="check-status">
+        <input type="submit" value="Check Status"></input>
+      </div>
+    )
+  }
