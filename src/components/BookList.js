ba1a6cd1e4003f10169b6ac5d51fac8552729f47
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-04-06 16:58:43 -0600

    fix linter

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -18,25 +18,24 @@ export default function BookList() {
     <div className="main-container">
       <div className="bookList-container">
         <div className="book-item">
-        {booklist.map((item) => (
+          {booklist.map((item) => (
           <li key={item.id}>
             {item.title}
-            <br></br>
+            <br />
             {item.Author}
-            <br></br>
-            <br></br>
+            <br />
+            <br />
           </li>
-        ),
-      )}
+        ),)}
         </div>
         <hr />
       </div>
       <div className="form-container">
         <h3 className="form-title">ADD NEW BOOK</h3>
         <form className="addabook">
-        <input type="text" name="title" placeholder="Book title" />
-        <input type="text" name="author" placeholder="Author" />
-        <input type="submit" value="ADD BOOK" />
+          <input type="text" name="title" placeholder="Book title" />
+          <input type="text" name="author" placeholder="Author" />
+          <input type="submit" value="ADD BOOK" />
         </form>
       </div>
     </div>
