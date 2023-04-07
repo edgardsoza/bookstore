87026c3f0306c705b04570195bb58441398532b9
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-04-06 19:18:44 -0600

    fix linter

--- src/components/BookItems.js
+++ src/components/BookItems.js
@@ -1,38 +1,38 @@
 import { FaTrash } from "react-icons/fa";
 
 const booklist = [
-    {
-      id: 1,
-      title: 'The Hunger Games',
-      Author: 'Suzanne Collins',
-    },
-    {
-      id: 2,
-      title: 'Dune',
-      Author: 'Frank Herbert',
-    },
-    {
-      id: 3,
-      title: 'Capital in the Twenty-First Century',
-      Author: 'Suzanne Collins',
-    },
-  ];
+  {
+    id: 1,
+    title: 'The Hunger Games',
+    Author: 'Suzanne Collins',
+  },
+  {
+    id: 2,
+    title: 'Dune',
+    Author: 'Frank Herbert',
+  },
+  {
+    id: 3,
+    title: 'Capital in the Twenty-First Century',
+    Author: 'Suzanne Collins',
+  },
+];
 
 export default function BookItem() {
     return (
     <ul className="book-item">
-    {booklist.map((item) => (
-      <li key={item.id}>
-        {item.title}
-        <br />
-        {item.Author}
-        <br />
-        <button type="submit"><FaTrash /></button>
-        <br />
-        <br />
-      </li>
-    ))}
-  <hr />
-  </ul>
-  )
+      {booklist.map((item) => (
+        <li key={item.id}>
+          {item.title}
+          <br />
+          {item.Author}
+          <br />
+          <button type="submit"><FaTrash /></button>
+          <br />
+          <br />
+        </li>
+      ))}
+        <hr />
+    </ul>
+  );
 }
